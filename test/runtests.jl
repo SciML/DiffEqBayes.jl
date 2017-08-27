86087308a115f15749bca09c193215e025b75fa8
Christopher Rackauckas <accounts@chrisrackauckas.com>
2017-08-26 21:08:49 -0700

    Update runtests.jl
    @Ayush-iitkgp this was failing on CI and one problem is sufficient to show it works, unless you were checking something extra with this other problem. So I think it's best to just delete it since as a chaotic problem Stan is going to have problems with it and we don't have the CI time for it.

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,23 +19,3 @@ priors = [Normal(1.5,1)]
 bayesian_result = bayesian_inference(prob1,t,data,priors;num_samples=100,num_warmup=100)
 theta1 = bayesian_result.chain_results[:,["theta.1"],:]
 @test mean(theta1.value[:,:,1]) ≈ 1.5 atol=1e-1
-
-
-g1 = @ode_def_nohes LorenzExample begin
-  dx = σ*(y-x)
-  dy = x*(ρ-z) - y
-  dz = x*y - β*z
-end σ=10.0 ρ=28.0 β=>2.6666
-
-r0 = [0.1;0.0;0.0]
-tspan = (0.0,4.0)
-prob = ODEProblem(g1,r0,tspan)
-sol = solve(prob,Tsit5())
-t = collect(linspace(0.1,4.0,10))
-randomized = VectorOfArray([(sol(t[i]) + .01randn(3)) for i in 1:length(t)])
-data = convert(Array,randomized)
-priors = [Normal(2.6666,1)]
-
-bayesian_result = bayesian_inference(prob,t,data,priors)
-theta1 = bayesian_result.chain_results[:,["theta.1"],:]
-@test mean(theta1.value[:,:,1]) ≈ 2.6666 atol=1e-1

6c8e41fe6b2f3d8d53ca5dae3998332c5e0460ad
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-01-13 11:01:24 -0800

    fix likelihood statement

--- src/stan_inference.jl
+++ src/stan_inference.jl
@@ -34,8 +34,8 @@ end
 function stan_inference(prob::DEProblem,t,data,priors = nothing;alg=:rk45,
                             num_samples=1000, num_warmup=1000, reltol=1e-3,
                             abstol=1e-6, maxiter=Int(1e5),likelihood=Normal,vars=("StanODEData",InverseGamma(2,3)))
-  length_of_y = string(length(prob.u0))
-  length_of_params = string(length(vars))
+  length_of_y = length(prob.u0)
+  length_of_params = length(vars)
   f = prob.f
   length_of_parameter = string(length(f.params))
   if alg ==:rk45
@@ -47,16 +47,18 @@ function stan_inference(prob::DEProblem,t,data,priors = nothing;alg=:rk45,
   end
   hyper_params = ""
   tuple_hyper_params = ""
+  setup_params = ""
   for i in 1:length(vars)
     if vars[i] == "StanODEData"
-      tuple_hyper_params = string(tuple_hyper_params,"u_hat[t]",",")
+      tuple_hyper_params = string(tuple_hyper_params,"u_hat[t,:]",",")
     else
       dist = stan_string(vars[i])
-      hyper_params = string(hyper_params,"params[$i] ~ $dist",";")
-      tuple_hyper_params = string(tuple_hyper_params,"params[$i]",",")
+      hyper_params = string(hyper_params,"sigma$(i-1) ~ $dist;")
+      tuple_hyper_params = string(tuple_hyper_params,"sigma$(i-1)",",")
+      setup_params = string(setup_params,"vector<lower=0>[$length_of_y] sigma$(i-1);")
     end
   end
-  tuple_hyper_params = tuple_hyper_params[1:endof(tuple_hyper_params)-1] 
+  tuple_hyper_params = tuple_hyper_params[1:endof(tuple_hyper_params)-1]
   differential_equation = generate_differential_equation(f)
   priors_string = string(generate_priors(f,priors),";")
   stan_likelihood = stan_string(likelihood)
@@ -80,9 +82,8 @@ function stan_inference(prob::DEProblem,t,data,priors = nothing;alg=:rk45,
     int x_i[0];
   }
   parameters {
-    vector<lower=0>[$length_of_y] sigma;
+    vector<lower=0>[$length_of_y] sigma1;
     real theta[$length_of_parameter];
-    real params[$length_of_params];
   }
   model{
     real u_hat[T,$length_of_y];
@@ -90,13 +91,12 @@ function stan_inference(prob::DEProblem,t,data,priors = nothing;alg=:rk45,
     $priors_string
     u_hat = $algorithm(sho, u0, t0, ts, theta, x_r, x_i, $reltol, $abstol, $maxiter);
     for (t in 1:T){
-      internal_var___u[t] ~ $stan_likelihood($tuple_hyper_params);
+      internal_var___u[t,:] ~ $stan_likelihood($tuple_hyper_params);
       }
   }
   "
-
   stanmodel = Stanmodel(num_samples=num_samples, num_warmup=num_warmup, name="parameter_estimation_model", model=parameter_estimation_model);
-  const parameter_estimation_data = Dict("u0"=>prob.u0, "T" => size(t)[1], "internal_var___u" => data', "t0" => prob.tspan[1], "ts" => t)
+  const parameter_estimation_data = Dict("u0"=>prob.u0, "T" => length(t), "internal_var___u" => data', "t0" => prob.tspan[1], "ts" => t)
   return_code, chain_results = stan(stanmodel, [parameter_estimation_data]; CmdStanDir=CMDSTAN_HOME)
   return StanModel(return_code,chain_results)
 end
--- test/stan.jl
+++ test/stan.jl
@@ -15,12 +15,13 @@ randomized = VectorOfArray([(sol(t[i]) + .01randn(2)) for i in 1:length(t)])
 data = convert(Array,randomized)
 priors = [Truncated(Normal(1.5,1),0,2)]
 
-bayesian_result = stan_inference(prob1,t,data,priors;num_samples=100,num_warmup=100,likelihood=Normal,vars =("StanODEData",InverseGamma(2,3)))
+bayesian_result = stan_inference(prob1,t,data,priors;num_samples=100,num_warmup=500,likelihood=Normal,vars =("StanODEData",InverseGamma(2,3)))
 theta1 = bayesian_result.chain_results[:,["theta.1"],:]
-@test mean(theta1.value[:,:,1]) ≈ 1.5 atol=1e-1
+@test mean(theta1.value) ≈ 1.5 atol=1e-1
+
 
 println("Four parameter case")
-f1 = @ode_def_nohes LotkaVolterraTest2 begin
+f1 = @ode_def_nohes LotkaVolterraTest4 begin
   dx = a*x - b*x*y
   dy = -c*y + d*x*y
 end a=>1.5 b=>1.0 c=>3.0 d=>1.0
@@ -31,8 +32,14 @@ sol = solve(prob1,Tsit5())
 t = collect(linspace(1,10,10))
 randomized = VectorOfArray([(sol(t[i]) + .01randn(2)) for i in 1:length(t)])
 data = convert(Array,randomized)
-priors = [Truncated(Normal(1.5,1),0,2),Truncated(Normal(1.0,1),0,2),Normal(3.0,1),Truncated(Normal(1.0,1),0,2)]
+priors = [Normal(1.5,0.01),Normal(1.0,0.01),Normal(3.0,0.01),Normal(1.0,0.01)]
 
-bayesian_result = stan_inference(prob1,t,data,priors;num_samples=100,num_warmup=100,vars =("StanODEData",InverseGamma(2,3)))
+bayesian_result = stan_inference(prob1,t,data,priors;num_samples=100,num_warmup=100,vars =("StanODEData",InverseGamma(4,1)))
 theta1 = bayesian_result.chain_results[:,["theta.1"],:]
+theta2 = bayesian_result.chain_results[:,["theta.2"],:]
+theta3 = bayesian_result.chain_results[:,["theta.3"],:]
+theta4 = bayesian_result.chain_results[:,["theta.4"],:]
 @test mean(theta1.value[:,:,1]) ≈ 1.5 atol=1e-1
+@test mean(theta2.value[:,:,1]) ≈ 1.0 atol=1e-1
+@test mean(theta3.value[:,:,1]) ≈ 3.0 atol=1e-1
+@test mean(theta4.value[:,:,1]) ≈ 1.0 atol=1e-1

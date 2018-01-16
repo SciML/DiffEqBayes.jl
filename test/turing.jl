2ae0237c4fa9f60c2e004ba25ca58b4efe28e680
Christopher Rackauckas <accounts@chrisrackauckas.com>
2018-01-16 04:29:39 -0800

    Update turing.jl

--- test/turing.jl
+++ test/turing.jl
@@ -14,6 +14,8 @@ randomized = VectorOfArray([(sol(t[i]) + .01randn(2)) for i in 1:length(t)])
 data = convert(Array,randomized)
 priors = [Normal(1.5,0.01)]
 
-bayesian_result = turing_inference(prob1,Tsit5(),t,data,priors;num_samples=100)
+bayesian_result = turing_inference(prob1,Tsit5(),t,data,priors;num_samples=500)
 
-@test mean(bayesian_result[:theta1][25:end]) ≈ 1.5 atol=0.1
+@show mean(bayesian_result[:theta1][50:end])
+
+@test mean(bayesian_result[:theta1][50:end]) ≈ 1.5 atol=0.1

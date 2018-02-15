5bda8a79353c9591b25caedf38fc5b22eb9d2226
Unknown <vaibhavyashdixit@gmail.com>
2018-02-15 23:17:13 +0530

    Extend plot_chain to Turing

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,8 @@
+get_plot_object(bayesian_result::StanModel) = Mamba.plot(bayesian_result.chain_results)
+get_plot_object(bayesian_result::Turing.Chain) = Mamba.plot(bayesian_result)
+
 function plot_chain(bayesian_result::StanModel,filename=nothing)
-	p = Mamba.plot(bayesian_result.chain_results)
+	p = get_plot_object(bayesian_result)
 	if filename == nothing
 		return Mamba.draw(p)
 	else

0d1c66e5116262502cd1d9bc417d1781d841f8c1
Mauro Werder <mauro_lc@runbox.com>
2018-05-02 20:32:54 +0200

    Made dynamichmc_inference more versatile: choise of alg, kwargs

--- src/dynamichmc_inference.jl
+++ src/dynamichmc_inference.jl
@@ -4,15 +4,17 @@ struct DynamicHMCPosterior
     a_prior
     t
     ϵ_dist
+    alg
+    kwargs
 end
 
 function (P::DynamicHMCPosterior)(θ)
     #println(θ)
-    @unpack problem, data, a_prior, t, ϵ_dist = P
+    @unpack problem, data, a_prior, t, ϵ_dist, alg, kwargs = P
     a = θ
     try
         prob = problem_new_parameters(problem, a)
-        sol = solve(prob, Tsit5())
+        sol = solve(prob, alg; kwargs...)
         ℓ = sum(sum(logpdf.(ϵ_dist, sol(t) .- data[:, i]))
                 for (i, t) in enumerate(t))
     catch
@@ -24,13 +26,14 @@ function (P::DynamicHMCPosterior)(θ)
     logpdf_sum = 0
     for i in length(a)
         logpdf_sum += logpdf(a_prior[i], a[i])
-    end 
+    end
     ℓ + logpdf_sum
 end
 
-function dynamichmc_inference(prob::DEProblem,data,priors,t,transformations;σ = 0.01,ϵ=0.001,initial=Float64[],iterations=1000)
-    P = DynamicHMCPosterior(prob, data, priors, t, Normal(0.0, σ))
-    
+function dynamichmc_inference(prob::DEProblem, alg, t, data, priors, transformations;
+                              σ=0.01, ϵ=0.001, initial=Float64[], iterations=1000, kwargs...)
+    P = DynamicHMCPosterior(prob, data, priors, t, Normal(0.0, σ), alg, kwargs)
+
     transformations_tuple = Tuple(transformations)
     parameter_transformation = TransformationTuple(transformations_tuple) # assuming a > 0
     PT = TransformLogLikelihood(P, parameter_transformation)
@@ -49,6 +52,7 @@ function dynamichmc_inference(prob::DEProblem,data,priors,t,transformations;σ =
         push!(lower_bound,minimum(i))
         push!(upper_bound,maximum(i))
     end
+
     optimized = Optim.minimizer(optimize(a -> -P(a),initial,lower_bound,upper_bound,Fminbox{GradientDescent}()))
     inverse_transforms = Float64[]
     for i in 1:length(initial)
@@ -56,11 +60,11 @@ function dynamichmc_inference(prob::DEProblem,data,priors,t,transformations;σ =
        push!(inverse_transforms,inverse(para, (optimized[i], ))[1])
     end
     #println(inverse_transforms)
-    sample, _ = NUTS_init_tune_mcmc(PTG,
-                                inverse_transforms,
-                                iterations,ϵ=ϵ)
-    
+    sample, NUTS_tuned = NUTS_init_tune_mcmc(PTG,
+                                         inverse_transforms,
+                                         iterations,ϵ=ϵ)
+
     posterior = ungrouping_map(Vector, get_transformation(PT) ∘ get_position, sample)
 
-    (posterior, sample, _)
+    return posterior, sample, NUTS_tuned
 end

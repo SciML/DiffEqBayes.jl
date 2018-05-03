d3764278bef38481eb8fe6d3de0cc3cb0c31590f
Mauro Werder <mauro_lc@runbox.com>
2018-05-03 18:07:35 +0200

    Added test for custom distance function for ABC

--- src/abc_inference.jl
+++ src/abc_inference.jl
@@ -1,22 +1,22 @@
-function createabcfunction(prob, t, distancefunction, alg, kwargs...)
-    function simfunc(params, constants, targetdata)
-        sol = solve(problem_new_parameters(prob, params), alg, saveat = t, save_start = false, kwargs...)
-        data = convert(Array, sol)
-        distancefunction(targetdata, data), data
+function createabcfunction(prob, t, distancefunction, alg; kwargs...)
+    function simfunc(params, constants, data)
+        sol = solve(problem_new_parameters(prob, params), alg; saveat = t, save_start = false, kwargs...)
+        simdata = convert(Array, sol)
+        distancefunction(data, simdata), nothing
     end
 end
 
 function abc_inference(prob::DEProblem, alg, t, data, priors; ϵ=0.001,
-     distancefunction = euclidean, ABCalgorithm = ABCSMC, progress = false,
-     num_samples = 500, maxiterations = 10^5, kwargs...)
+                       distancefunction = euclidean, ABCalgorithm = ABCSMC, progress = false,
+                       num_samples = 500, maxiterations = 10^5, kwargs...)
 
-    abcsetup = ABCalgorithm(createabcfunction(prob, t, distancefunction, alg, kwargs...),
-     length(priors),
-     ϵ,
-     ApproxBayes.Prior(priors);
-     nparticles = num_samples,
-     maxiterations = maxiterations
-    )
+    abcsetup = ABCalgorithm(createabcfunction(prob, t, distancefunction, alg; kwargs...),
+                            length(priors),
+                            ϵ,
+                            ApproxBayes.Prior(priors);
+                            nparticles = num_samples,
+                            maxiterations = maxiterations
+                            )
 
     abcresult = runabc(abcsetup, data, progress = progress)
     return abcresult

adbd954def371071ddfb63658c562111b2f2842f
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-09-17 13:06:47 -0700

    fix tictoc

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
 using DiffEqBayes
 using Test
 
-tic()
+@time begin
 @time @testset "DynamicHMC" begin include("dynamicHMC.jl") end
 @time @testset "Stan_String" begin include("stan_string.jl") end
 @time @testset "Stan" begin include("stan.jl") end
 @time @testset "Turing" begin include("turing.jl") end # Doesn't work on v0.6
 @time @testset "ABC" begin include("abc.jl") end
-toc()
+end

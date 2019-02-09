61da1eacf660ec4aa455590e02e6cd3a9801ca0c
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2019-02-09 16:06:25 +0530

    Update turing_inference #46

--- test/turing.jl
+++ test/turing.jl
@@ -16,9 +16,9 @@ priors = [Normal(1.5,0.01)]
 
 bayesian_result = turing_inference(prob1,Tsit5(),t,data,priors;num_samples=500)
 
-@show mean(bayesian_result[:theta1][50:end])
+@show mean(bayesian_result[:_theta][50:end])
 
-@test mean(bayesian_result[:theta1][50:end]) ≈ 1.5 atol=0.1
+@test mean(bayesian_result[:_theta1][50:end])[1] ≈ 1.5 atol=0.1
 
 println("Four parameter case")
 f1 = @ode_def begin
@@ -38,12 +38,9 @@ priors = [Truncated(Normal(1.5,0.01),0,2),Truncated(Normal(1.0,0.01),0,1.5),
 
 bayesian_result = turing_inference(prob1,Tsit5(),t,data,priors;num_samples=500)
 
-@show mean(bayesian_result[:theta1][50:end])
-@show mean(bayesian_result[:theta2][50:end])
-@show mean(bayesian_result[:theta3][50:end])
-@show mean(bayesian_result[:theta4][50:end])
+@show mean(bayesian_result[:_theta][50:end])
 
-@test mean(bayesian_result[:theta1][50:end]) ≈ 1.5 atol=3e-1
-@test mean(bayesian_result[:theta2][50:end]) ≈ 1.0 atol=3e-1
-@test mean(bayesian_result[:theta3][50:end]) ≈ 3.0 atol=3e-1
-@test mean(bayesian_result[:theta4][50:end]) ≈ 1.0 atol=3e-1
+@test mean(bayesian_result[:_theta][50:end])[1] ≈ 1.5 atol=3e-1
+@test mean(bayesian_result[:_theta][50:end])[2] ≈ 1.0 atol=3e-1
+@test mean(bayesian_result[:_theta][50:end])[3] ≈ 3.0 atol=3e-1
+@test mean(bayesian_result[:_theta][50:end])[4] ≈ 1.0 atol=3e-1

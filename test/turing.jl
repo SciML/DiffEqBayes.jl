db74d831e0f6512ba38825684ceb5121a99aadff
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2019-05-29 12:38:07 +0530

    Fix Turing test

--- test/turing.jl
+++ test/turing.jl
@@ -16,9 +16,9 @@ priors = [Normal(1.5,0.01)]
 
 bayesian_result = turing_inference(prob1,Tsit5(),t,data,priors;num_samples=500)
 
-@show mean(bayesian_result[:_theta][50:end])
+@show bayesian_result 
 
-@test mean(bayesian_result[:_theta][50:end])[1] ≈ 1.5 atol=0.1
+@test mean(bayesian_result.value[:,1,1]) ≈ 1.5 atol=0.1
 
 println("Four parameter case")
 f2 = @ode_def begin
@@ -38,9 +38,9 @@ priors = [Truncated(Normal(1.5,0.01),0,2),Truncated(Normal(1.0,0.01),0,1.5),
 
 bayesian_result = turing_inference(prob2,Tsit5(),t,data,priors;num_samples=500)
 
-@show mean(bayesian_result[:_theta][:,:][50:end])
+@show bayesian_result
 
-@test mean(bayesian_result[:_theta][:,1][50:end]) ≈ 1.5 atol=3e-1
-@test mean(bayesian_result[:_theta][:,2][50:end]) ≈ 1.0 atol=3e-1
-@test mean(bayesian_result[:_theta][:,3][50:end]) ≈ 3.0 atol=3e-1
-@test mean(bayesian_result[:_theta][:,4][50:end]) ≈ 1.0 atol=3e-1
+@test mean(bayesian_result.value[:,1,1]) ≈ 1.5 atol=3e-1
+@test mean(bayesian_result.value[:,2,1]) ≈ 1.0 atol=3e-1
+@test mean(bayesian_result.value[:,3,1]) ≈ 3.0 atol=3e-1
+@test mean(bayesian_result.value[:,4,1]) ≈ 1.0 atol=3e-1

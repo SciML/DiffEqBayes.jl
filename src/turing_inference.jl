873a7f1fc8781b816c65d1ae072b6c14c53fb416
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-06-21 08:10:56 -0400

    generalize the likelihood definition in turing inference

--- src/turing_inference.jl
+++ src/turing_inference.jl
@@ -1,16 +1,23 @@
 function turing_inference(prob::DiffEqBase.DEProblem,alg,t,data,priors = nothing;
+  likelihood_dist_priors = [InverseGamma(2, 3)],
+  likelihood = (u,p,t,σ) -> MvNormal(u, σ[1]*ones(length(u))),
   num_samples=1000, delta=0.65, kwargs...)
 
   N = length(priors)
   _theta = Vector{Real}(undef, N)
+  _σ = Vector{Real}(undef, length(likelihood_dist_priors))
   @model bif(x) = begin
     for i in 1:length(priors)
       _theta[i] ~ priors[i]
     end
-    σ ~ InverseGamma(2, 3)
+    for i in 1:length(likelihood_dist_priors)
+      _σ[i] ~ InverseGamma(2, 3)
+    end
 
     theta = convert(Array{typeof(first(_theta))}, _theta)
-    p_tmp = remake(prob, u0 = convert.(eltype(theta), (prob.u0)), p = theta) 
+    σ = convert(Array{typeof(first(_σ))}, _σ)
+
+    p_tmp = remake(prob, u0 = convert.(eltype(theta), (prob.u0)), p = theta)
     sol_tmp = solve(p_tmp, alg; saveat = t, kwargs...)
     fill_length = length(t) - length(sol_tmp.u)
 
@@ -23,7 +30,8 @@ function turing_inference(prob::DiffEqBase.DEProblem,alg,t,data,priors = nothing
     end
     for i = 1:length(t)
       res = sol_tmp.u[i]
-      x[:,i] ~ MvNormal(res, σ*ones(length(prob.u0)))
+      _t   = sol_tmp.t[i]
+      x[:,i] ~ likelihood(res,theta,_t,σ)
     end
   end
 

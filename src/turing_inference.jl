1c759385c1e61ae14fa4f5bdca74832489206d34
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-06-21 13:51:00 -0400

    Switch away from macro

--- src/turing_inference.jl
+++ src/turing_inference.jl
@@ -1,21 +1,40 @@
-function turing_inference(prob::DiffEqBase.DEProblem,alg,t,data,priors = nothing;
+function turing_inference(prob::DiffEqBase.DEProblem,alg,t,data,priors;
   likelihood_dist_priors = [InverseGamma(2, 3)],
   likelihood = (u,p,t,σ) -> MvNormal(u, σ[1]*ones(length(u))),
   num_samples=1000, sampler = Turing.NUTS(num_samples, 0.65),
+  syms = [Symbol("theta$i") for i in 1:length(priors)],
   kwargs...)
 
   N = length(priors)
-  @model bif(x) = begin
-    val ~ priors[1]
-    theta = Vector{typeof(val)}(undef,length(priors))
+  mf(vi,sampler,model) = begin
+    # If x is provided, use the provided values.
+    # Otherwise, treat x as an empty vector with
+    # two entries.
+    x = model.defaults.x
+
+    N = length(priors)
+    _theta = Vector(undef,length(priors))
+
     for i in 1:length(priors)
-      theta[i] ~ priors[i]
+      _theta[i], __lp = Turing.assume(sampler,
+                        priors[i],
+                        Turing.VarName([:mf, syms[i]], ""),
+                        vi)
+      vi.logp += __lp
     end
-    val2 ~ likelihood_dist_priors[1]
-    σ = Vector{typeof(val)}(undef,length(priors))
+
+    theta = convert(Array{typeof(first(_theta))},_theta)
+    _σ = Vector(undef,length(likelihood_dist_priors))
+
     for i in 1:length(likelihood_dist_priors)
-      σ[i] ~ likelihood_dist_priors[i]
+      _σ[i], __lp = Turing.assume(sampler,
+                      likelihood_dist_priors[i],
+                      Turing.VarName([:mf, Symbol("σ$i")], ""),
+                      vi)
+      vi.logp += __lp
     end
+    σ = convert(Array{typeof(first(_σ))},_σ)
+
     p_tmp = remake(prob, u0 = convert.(eltype(theta), (prob.u0)), p = theta)
     sol_tmp = solve(p_tmp, alg; saveat = t, kwargs...)
 
@@ -26,15 +45,31 @@ function turing_inference(prob::DiffEqBase.DEProblem,alg,t,data,priors = nothing
       else
         push!(sol_tmp.u, fill(Inf, size(sol_tmp[1])))
       end
+      push!(sol_tmp.t, Inf)
     end
 
     for i = 1:length(t)
       res = sol_tmp.u[i]
       _t   = sol_tmp.t[i]
-      x[:,i] ~ likelihood(res,theta,_t,σ)
+      __lp = Turing.observe(
+        sampler,
+        likelihood(res,theta,_t,σ),   # Distribution
+        x[:,i],    # Data point
+        vi
+      )
+      vi.logp += __lp
     end
+
+    vi
   end
 
-  model = bif(data)
+  bigtup = Tuple{([[syms[i] for i in 1:length(priors)]; [Symbol("σ$i") for i in 1:length(likelihood_dist_priors)]])...}
+
+  # Define the default value for x when missing
+  defaults = (x = data,)
+
+  # Instantiate a Model object.
+  model = Turing.Model{bigtup, Tuple{:x}}(mf, data, defaults)
+
   chn = sample(model, sampler)
 end

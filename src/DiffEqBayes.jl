9244bae068a59d76cc047771b49f24f631d0f5a3
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-25 06:56:53 -0400

    fix require

--- src/DiffEqBayes.jl
+++ src/DiffEqBayes.jl
@@ -14,7 +14,7 @@ include("dynamichmc_inference.jl")
 include("abc_inference.jl")
 
 function __init__()
-    @requires Stan="593b3428-ca2f-500c-ae53-031589ec8ddd" begin
+    @require Stan="593b3428-ca2f-500c-ae53-031589ec8ddd" begin
         include("stan_inference.jl")
         include("stan_string.jl")
         export StanModel, stan_inference

7cedae26634d1f3171c51e25b1712f06e9d9b428
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-28 15:31:17 -0400

    move usings to Requires

--- src/DiffEqBayes.jl
+++ src/DiffEqBayes.jl
@@ -1,9 +1,8 @@
 module DiffEqBayes
 using DiffEqBase, CmdStan, Distributions, Turing, MacroTools, Mamba
 using ParameterizedFunctions, RecursiveArrayTools
-#using DynamicHMC, TransformVariables, LogDensityProblems
 using Parameters, Distributions, Optim, Requires
-using Distances, ApproxBayes, StatsPlots
+using Distances, ApproxBayes
 
 STANDARD_PROB_GENERATOR(prob,p) = remake(prob;u0=eltype(p).(prob.u0),p=p)
 STANDARD_PROB_GENERATOR(prob::EnsembleProblem,p) = EnsembleProblem(remake(prob.prob;u0=eltype(p).(prob.prob.u0),p=p))
@@ -19,6 +18,7 @@ function __init__()
     end
 
     @require DynamicHMC="bbc10e6e-7c05-544b-b16e-64fede858acb" begin
+        using DynamicHMC, TransformVariables, LogDensityProblems
         include("dynamichmc_inference.jl")
         export dynamichmc_inference
     end

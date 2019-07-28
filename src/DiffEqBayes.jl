d69ffc2b3afb80b7c29f033476a1e699e78ad648
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-28 14:52:58 -0400

    further dependency reduction

--- src/DiffEqBayes.jl
+++ src/DiffEqBayes.jl
@@ -9,8 +9,6 @@ STANDARD_PROB_GENERATOR(prob,p) = remake(prob;u0=eltype(p).(prob.u0),p=p)
 STANDARD_PROB_GENERATOR(prob::EnsembleProblem,p) = EnsembleProblem(remake(prob.prob;u0=eltype(p).(prob.prob.u0),p=p))
 
 include("turing_inference.jl")
-include("utils.jl")
-#include("dynamichmc_inference.jl")
 include("abc_inference.jl")
 
 function __init__()
@@ -19,8 +17,18 @@ function __init__()
         include("stan_string.jl")
         export StanModel, stan_inference, stan_string, StanODEData
     end
+
+    @require DynamicHMC="bbc10e6e-7c05-544b-b16e-64fede858acb" begin
+        include("dynamichmc_inference.jl")
+        export dynamichmc_inference
+    end
+
+    @require StatsPlots="f3b207a7-027a-5e70-b257-86293d7955fd" begin
+        include("utils.jl")
+        export plot_chain
+    end
 end
 
-export turing_inference, plot_chain, dynamichmc_inference, abc_inference
+export turing_inference, abc_inference
 
 end # module

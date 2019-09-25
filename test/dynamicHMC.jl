1d0ad5d09fb4ac3e19773708c7edc26bc3065fa7
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2019-09-26 04:03:23 +0530

    reduce variance for 4 parameter case

--- test/dynamicHMC.jl
+++ test/dynamicHMC.jl
@@ -50,10 +50,10 @@ sol = solve(prob1,Tsit5())
 t = collect(range(1,stop=10,length=10))
 randomized = VectorOfArray([(sol(t[i]) + .01randn(2)) for i in 1:length(t)])
 data = convert(Array,randomized)
-priors = (a = Truncated(Normal(1.5,0.1), 0, 2),
-              b = Truncated(Normal(1.0,0.1), 0, 1.5),
-              c = Truncated(Normal(3.0,0.1), 0, 4),
-              d = Truncated(Normal(1.0, 0.1), 0, 2))
+priors = (a = Truncated(Normal(1.5,0.01), 0, 2),
+              b = Truncated(Normal(1.0,0.01), 0, 1.5),
+              c = Truncated(Normal(3.0,0.01), 0, 4),
+              d = Truncated(Normal(1.0, 0.01), 0, 2))
 bayesian_result = dynamichmc_inference(prob1, Tsit5(), t, data, priors,
           as(Vector, asℝ₊, 4))
 @test norm(mean([p.parameters for p in bayesian_result.posterior]) .- p, Inf) ≤ 0.1

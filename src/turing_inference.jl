b2bff72c3942157c273bc904a9dfdd4d51add616
mohamed82008 <mohamed82008@gmail.com>
2020-01-25 11:55:53 +1100

    fix Turing

--- src/turing_inference.jl
+++ src/turing_inference.jl
@@ -1,86 +1,55 @@
-function turing_inference(prob::DiffEqBase.DEProblem,alg,t,data,priors;
-  likelihood_dist_priors = [InverseGamma(2, 3)],
-  likelihood = (u,p,t,σ) -> MvNormal(u, σ[1]*ones(length(u))),
-  num_samples=1000, sampler = Turing.NUTS(0.65),
-  syms = [Symbol("theta$i") for i in 1:length(priors)],
-  kwargs...)
-
-  N = length(priors)
-  mf(vi,sampler,model) = begin
-    # If x is provided, use the provided values.
-    # Otherwise, treat x as an empty vector with
-    # two entries.
-    x = model.defaults.x
-
+function turing_inference(
+    prob::DiffEqBase.DEProblem,
+    alg,
+    t,
+    data,
+    priors;
+    likelihood_dist_priors = [InverseGamma(2, 3)],
+    likelihood = (u,p,t,σ) -> MvNormal(u, σ[1]*ones(length(u))),
+    num_samples=1000, sampler = Turing.NUTS(0.65),
+    syms = [Turing.@varname(theta[i]) for i in 1:length(priors)],
+    kwargs...,
+)
     N = length(priors)
-    _theta = Vector(undef,length(priors))
-
-    for i in 1:length(priors)
-      _theta[i], __lp = Turing.assume(sampler,
-                        priors[i],
-                        Turing.VarName([:mf, syms[i]], ""),
-                        vi)
-      vi.logp += __lp
-    end
-
-    theta = convert(Array{typeof(first(_theta))},_theta)
-    _σ = Vector(undef,length(likelihood_dist_priors))
-
-    for i in 1:length(likelihood_dist_priors)
-      _σ[i], __lp = Turing.assume(sampler,
-                      likelihood_dist_priors[i],
-                      Turing.VarName([:mf, Symbol("σ$i")], ""),
-                      vi)
-      vi.logp += __lp
-    end
-    σ = convert(Array{typeof(first(_σ))},_σ)
-
-    p_tmp = remake(prob, u0 = convert.(eltype(theta), (prob.u0)), p = theta)
-    _saveat = t === nothing ? Float64[] : t
-    sol_tmp = solve(p_tmp, alg; saveat = _saveat, kwargs...)
-
-    if sol_tmp isa DiffEqBase.AbstractEnsembleSolution
-      failure = any((s.retcode != :Success for s in sol_tmp)) && any((s.retcode != :Terminated for s in sol_tmp))
-    else
-      failure = sol_tmp.retcode != :Success && sol_tmp != :Terminated
-    end
-
-    if failure
-      vi.logp -= Inf
-    else
-      if sol_tmp isa DiffEqBase.AbstractNoTimeSolution
-        res = sol_tmp.u
-        __lp = Turing.observe(
-          sampler,
-          likelihood(res,theta,Inf,σ),   # Distribution
-          x,    # Data point
-          vi
-        )
-        vi.logp += __lp
-      else
-        for i = 1:length(t)
-          res = sol_tmp.u[i]
-          _t   = sol_tmp.t[i]
-          __lp = Turing.observe(
-            sampler,
-            likelihood(res,theta,_t,σ),   # Distribution
-            x[:,i],    # Data point
-            vi
-          )
-          vi.logp += __lp
+    Turing.@model mf(x, ::Type{T} = Float64) where {T <: Real} = begin
+        theta = Vector{T}(undef, length(priors))
+        for i in 1:length(priors)
+            theta[i] ~ NamedDist(priors[i], syms[i])
         end
-      end
+        σ = Vector{T}(undef, length(likelihood_dist_priors))
+        for i in 1:length(likelihood_dist_priors)
+            σ[i] ~ likelihood_dist_priors[i]
+        end
+        p_tmp = remake(prob, u0 = convert.(T, (prob.u0)), p = theta)
+        _saveat = t === nothing ? Float64[] : t
+        sol_tmp = solve(p_tmp, alg; saveat = _saveat, kwargs...)
+
+        if sol_tmp isa DiffEqBase.AbstractEnsembleSolution
+            failure = any((s.retcode != :Success for s in sol_tmp)) && any((s.retcode != :Terminated for s in sol_tmp))
+        else
+            failure = sol_tmp.retcode != :Success && sol_tmp != :Terminated
+        end
+
+        if failure
+            @logpdf() = -Inf
+            return
+        end
+        if sol_tmp isa DiffEqBase.AbstractNoTimeSolution
+            res = sol_tmp.u
+            x ~ likelihood(res, theta, Inf, σ)
+        else
+            u = sol_tmp.u
+            for i = 1:length(t)
+                res = sol_tmp.u[i]
+                _t = sol_tmp.t[i]
+                x[:,i] ~ likelihood(res, theta, _t, σ)
+            end
+        end
+        return 
     end
-    vi
-  end
 
-  bigtup = Tuple{([[syms[i] for i in 1:length(priors)]; [Symbol("σ$i") for i in 1:length(likelihood_dist_priors)]])...}
-
-  # Define the default value for x when missing
-  defaults = (x = data,)
-
-  # Instantiate a Model object.
-  model = Turing.Model{bigtup, Tuple{:x}}(mf, data, defaults)
-
-  chn = sample(model, sampler, num_samples)
+    # Instantiate a Model object.
+    model = mf(data)
+    chn = sample(model, sampler, num_samples)
+    return chn
 end

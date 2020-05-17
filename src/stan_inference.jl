5f2ff705c7af35a715a9a1be2f6ac9db27ce8fff
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2020-05-17 11:54:21 -0400

    Update stan_inference.jl

--- src/stan_inference.jl
+++ src/stan_inference.jl
@@ -46,11 +46,13 @@ function generate_theta(n,priors)
   return theta
 end
 
-function stan_inference(prob::DiffEqBase.DEProblem,t,data,priors = nothing;alg=:rk45,
+function stan_inference(prob::DiffEqBase.DEProblem,t,data,priors = nothing,
+                            stanmodel = nothing;alg=:rk45,
                             num_samples=1000, num_warmup=1000, reltol=1e-3,
                             abstol=1e-6, maxiter=Int(1e5),likelihood=Normal,
                             vars=(StanODEData(),InverseGamma(3,3)),nchains=1,
                             sample_u0 = false, save_idxs = nothing, diffeq_string = nothing, printsummary = true)
+  
   save_idxs !== nothing && length(save_idxs) == 1 ? save_idxs = save_idxs[1] : save_idxs = save_idxs
   length_of_y = length(prob.u0)
   save_idxs = something(save_idxs, 1:length_of_y)
@@ -61,109 +63,113 @@ function stan_inference(prob::DiffEqBase.DEProblem,t,data,priors = nothing;alg=:
   else
     length_of_parameter = length(prob.p) + sample_u0 * length(save_idxs)
   end
-  if alg ==:rk45
-    algorithm = "integrate_ode_rk45"
-  elseif alg == :bdf
-    algorithm = "integrate_ode_bdf"
-  else
-    error("The choices for alg are :rk45 or :bdf")
-  end
-  hyper_params = ""
-  tuple_hyper_params = ""
-  setup_params = ""
-  thetas = ""
-  theta_string = generate_theta(length_of_parameter,priors)
-  for i in 1:length_of_parameter
-    thetas = string(thetas,"theta[$i] = theta$i",";")
-  end
-  for i in 1:length_of_params
-    if isa(vars[i],StanODEData)
-      tuple_hyper_params = string(tuple_hyper_params,"u_hat[t,$save_idxs]",",")
+  
+  if stanmodel === nothing
+    if alg ==:rk45
+      algorithm = "integrate_ode_rk45"
+    elseif alg == :bdf
+      algorithm = "integrate_ode_bdf"
     else
-      dist = stan_string(vars[i])
-      hyper_params = string(hyper_params,"sigma$(i-1) ~ $dist;")
-      tuple_hyper_params = string(tuple_hyper_params,"sigma$(i-1)",",")
-      setup_params = string(setup_params,"row_vector<lower=0>[$(length(save_idxs))] sigma$(i-1);")
+      error("The choices for alg are :rk45 or :bdf")
     end
-  end
-  tuple_hyper_params = tuple_hyper_params[1:length(tuple_hyper_params)-1]
-  priors_string = string(generate_priors(length_of_parameter,priors))
-  stan_likelihood = stan_string(likelihood)
-  if sample_u0
-    nu = length(save_idxs)
-    if nu < length(prob.u0)
-      u0 = "{"
-      for u_ in prob.u0[nu+1:length(prob.u0)]
-        u0 = u0*string(u_)
+    hyper_params = ""
+    tuple_hyper_params = ""
+    setup_params = ""
+    thetas = ""
+    theta_string = generate_theta(length_of_parameter,priors)
+    for i in 1:length_of_parameter
+      thetas = string(thetas,"theta[$i] = theta$i",";")
+    end
+    for i in 1:length_of_params
+      if isa(vars[i],StanODEData)
+        tuple_hyper_params = string(tuple_hyper_params,"u_hat[t,$save_idxs]",",")
+      else
+        dist = stan_string(vars[i])
+        hyper_params = string(hyper_params,"sigma$(i-1) ~ $dist;")
+        tuple_hyper_params = string(tuple_hyper_params,"sigma$(i-1)",",")
+        setup_params = string(setup_params,"row_vector<lower=0>[$(length(save_idxs))] sigma$(i-1);")
       end
-      u0 = u0*"}"
-      integral_string = "u_hat = $algorithm(sho, append_array(theta[1:$nu],$u0), t0, ts, theta[$(nu+1):$length_of_parameter], x_r, x_i, $reltol, $abstol, $maxiter);"
-    else 
-      integral_string = "u_hat = $algorithm(sho, theta[1:$nu], t0, ts, theta[$(nu+1):$length_of_parameter], x_r, x_i, $reltol, $abstol, $maxiter);"
     end
-  else
-    integral_string = "u_hat = $algorithm(sho, u0, t0, ts, theta, x_r, x_i, $reltol, $abstol, $maxiter);"
+    tuple_hyper_params = tuple_hyper_params[1:length(tuple_hyper_params)-1]
+    priors_string = string(generate_priors(length_of_parameter,priors))
+    stan_likelihood = stan_string(likelihood)
+    if sample_u0
+      nu = length(save_idxs)
+      if nu < length(prob.u0)
+        u0 = "{"
+        for u_ in prob.u0[nu+1:length(prob.u0)]
+          u0 = u0*string(u_)
+        end
+        u0 = u0*"}"
+        integral_string = "u_hat = $algorithm(sho, append_array(theta[1:$nu],$u0), t0, ts, theta[$(nu+1):$length_of_parameter], x_r, x_i, $reltol, $abstol, $maxiter);"
+      else 
+        integral_string = "u_hat = $algorithm(sho, theta[1:$nu], t0, ts, theta[$(nu+1):$length_of_parameter], x_r, x_i, $reltol, $abstol, $maxiter);"
+      end
+    else
+      integral_string = "u_hat = $algorithm(sho, u0, t0, ts, theta, x_r, x_i, $reltol, $abstol, $maxiter);"
+    end
+    binsearch_string = """
+      int bin_search(real x, int min_val, int max_val){
+        int range = (max_val - min_val + 1) / 2;
+        int mid_pt = min_val + range;
+        int out;
+        while (range > 0) {
+            if (x == mid_pt) {
+                out = mid_pt;
+                range = 0;
+            } else {
+                range = (range + 1) / 2; 
+                mid_pt = x > mid_pt ? mid_pt + range: mid_pt - range; 
+            }
+        }
+        return out;
+    }
+    """
+    if isnothing(diffeq_string)
+      diffeq_string = ModelingToolkit.build_function(
+          sys.eqs,sys.states,
+          sys.ps,sys.iv,
+          fname = :sho,
+          target = ModelingToolkit.StanTarget()
+      )
+    end
+    parameter_estimation_model = "
+    functions {
+      $binsearch_string
+      $diffeq_string
+    }
+    data {
+      real u0[$length_of_y];
+      int<lower=1> T;
+      real internal_var___u[T,$(length(save_idxs))];
+      real t0;
+      real ts[T];
+    }
+    transformed data {
+      real x_r[0];
+      int x_i[0];
+    }
+    parameters {
+      $setup_params
+      $theta_string
+    }
+    transformed parameters{
+      real theta[$length_of_parameter];
+      $thetas
+    }
+    model{
+      real u_hat[T,$length_of_y];
+      $hyper_params
+      $priors_string
+      $integral_string
+      for (t in 1:T){
+        internal_var___u[t,:] ~ $stan_likelihood($tuple_hyper_params);
+        }
+    }
+    "
+    stanmodel = CmdStan.Stanmodel(num_samples=num_samples, num_warmup=num_warmup, name="parameter_estimation_model", model=parameter_estimation_model, nchains=nchains, printsummary = printsummary)
   end
-  binsearch_string = """
-    int bin_search(real x, int min_val, int max_val){
-      int range = (max_val - min_val + 1) / 2;
-      int mid_pt = min_val + range;
-      int out;
-      while (range > 0) {
-          if (x == mid_pt) {
-              out = mid_pt;
-              range = 0;
-          } else {
-              range = (range + 1) / 2; 
-              mid_pt = x > mid_pt ? mid_pt + range: mid_pt - range; 
-          }
-      }
-      return out;
-  }
-  """
-  if isnothing(diffeq_string)
-    diffeq_string = ModelingToolkit.build_function(
-        sys.eqs,sys.states,
-        sys.ps,sys.iv,
-        fname = :sho,
-        target = ModelingToolkit.StanTarget()
-    )
-  end
-  parameter_estimation_model = "
-  functions {
-    $binsearch_string
-    $diffeq_string
-  }
-  data {
-    real u0[$length_of_y];
-    int<lower=1> T;
-    real internal_var___u[T,$(length(save_idxs))];
-    real t0;
-    real ts[T];
-  }
-  transformed data {
-    real x_r[0];
-    int x_i[0];
-  }
-  parameters {
-    $setup_params
-    $theta_string
-  }
-  transformed parameters{
-    real theta[$length_of_parameter];
-    $thetas
-  }
-  model{
-    real u_hat[T,$length_of_y];
-    $hyper_params
-    $priors_string
-    $integral_string
-    for (t in 1:T){
-      internal_var___u[t,:] ~ $stan_likelihood($tuple_hyper_params);
-      }
-  }
-  "
-  stanmodel = CmdStan.Stanmodel(num_samples=num_samples, num_warmup=num_warmup, name="parameter_estimation_model", model=parameter_estimation_model, nchains=nchains, printsummary = printsummary)
+  
   parameter_estimation_data = Dict("u0"=>prob.u0, "T" => length(t), "internal_var___u" => view(data, :, 1:length(t))', "t0" => prob.tspan[1], "ts" => t)
   return_code, chains, cnames = CmdStan.stan(stanmodel, [parameter_estimation_data]; CmdStanDir=CMDSTAN_HOME)
   return StanModel(stanmodel, return_code, chains, cnames)

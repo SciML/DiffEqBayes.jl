65e011ddf328d87065dc8984880895979878c659
maja.k.gwozdz@gmail.com <maja.k.gwozdz@gmail.com>
2020-11-15 16:10:36 +0100

    Update make.jl
    Added URL and authors.

--- docs/make.jl
+++ docs/make.jl
@@ -2,13 +2,13 @@ using Documenter, DiffEqBayes
 
 makedocs(
     sitename="DiffEqBayes.jl",
-    authors="#",
+    authors="Chris Rackauckas et al.",
     clean=true,
     doctest=false,
     modules=[DiffEqBayes],
 
     format=Documenter.HTML(assets=["assets/favicon.ico"],
-                           canonical="#"),
+                           canonical="https://diffeqbayes.sciml.ai/stable/"),
 
     pages=[
         "DiffEqBayes.jl: Bayesian Parameter Estimation for Differential Equations" => "index.md",

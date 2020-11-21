9af0225266aa3f22f941c4ea8f261310aeae99e4
Vaibhav Kumar Dixit <vaibhavyashdixit@gmail.com>
2020-11-21 10:32:18 +0530

    Add parallel kwarg to abc_inference

--- src/abc_inference.jl
+++ src/abc_inference.jl
@@ -23,7 +23,7 @@ end
 
 function abc_inference(prob::DiffEqBase.DEProblem, alg, t, data, priors; ϵ=0.001,
                        distancefunction = euclidean, ABCalgorithm = ABCSMC, progress = false,
-                       num_samples = 500, maxiterations = 10^5, save_idxs = nothing, sample_u0 = false, kwargs...)
+                       num_samples = 500, maxiterations = 10^5, save_idxs = nothing, sample_u0 = false, parallel = false, kwargs...)
 
     abcsetup = ABCalgorithm(createabcfunction(prob, t, distancefunction, alg; save_idxs = save_idxs, sample_u0 = sample_u0, kwargs...),
                             length(priors),
@@ -33,6 +33,6 @@ function abc_inference(prob::DiffEqBase.DEProblem, alg, t, data, priors; ϵ=0.00
                             maxiterations = maxiterations
                             )
 
-    abcresult = runabc(abcsetup, data, progress = progress)
+    abcresult = runabc(abcsetup, data, progress = progress, parallel = parallel)
     return abcresult
 end

e32d8bcb67b1c3c5c204dcf041d18857cc9c6503
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2021-04-23 17:18:56 +0530

    Update cmdstan version

--- .github/scripts/stan.sh
+++ .github/scripts/stan.sh
@@ -1,7 +1,7 @@
-JULIA_CMDSTAN_HOME="$HOME/cmdstan-2.20.0/"
+JULIA_CMDSTAN_HOME="$HOME/cmdstan-2.26.1/"
 OLDWD=`pwd`
 cd ~
-wget https://github.com/stan-dev/cmdstan/releases/download/v2.20.0/cmdstan-2.20.0.tar.gz
-tar -xzpf cmdstan-2.20.0.tar.gz
+wget https://github.com/stan-dev/cmdstan/releases/download/v2.26.1/cmdstan-2.26.1.tar.gz
+tar -xzpf cmdstan-2.26.1.tar.gz
 make -C $JULIA_CMDSTAN_HOME build
 cd $OLDWD

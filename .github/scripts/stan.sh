c8a192343ecb53611104b248b1e55c977676e54a
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2021-04-23 12:32:50 +0530

    Update runtests and CI.yml for running stan test on CI

---
+++ .github/scripts/stan.sh
@@ -0,0 +1,7 @@
+JULIA_CMDSTAN_HOME="$HOME/cmdstan-2.20.0/"
+OLDWD=`pwd`
+cd ~
+wget https://github.com/stan-dev/cmdstan/releases/download/v2.20.0/cmdstan-2.20.0.tar.gz
+tar -xzpf cmdstan-2.20.0.tar.gz
+make -C $JULIA_CMDSTAN_HOME build
+cd $OLDWD
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,22 +2,18 @@ using SafeTestsets
 const LONGER_TESTS = false
 
 const GROUP = get(ENV, "GROUP", "All")
-const is_APPVEYOR = Sys.iswindows() && haskey(ENV,"APPVEYOR")
-const is_TRAVIS = haskey(ENV,"TRAVIS")
 
-@time begin
+
+
 if GROUP == "All" || GROUP == "Core"
     @time @safetestset "DynamicHMC" begin include("dynamicHMC.jl") end
     @time @safetestset "Turing" begin include("turing.jl") end
     @time @safetestset "ABC" begin include("abc.jl") end
 end
-end
 
-if GROUP == "Stan"
-    if is_TRAVIS
-      using Pkg
-      Pkg.add("CmdStan")
-    end
+if GROUP == "Stan" || GROUP == "All"
+    using Pkg
+    Pkg.add("CmdStan")
     @time @safetestset "Stan_String" begin include("stan_string.jl") end
     @time @safetestset "Stan" begin include("stan.jl") end
 end

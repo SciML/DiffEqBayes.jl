5e407920d2f962acfb5882e675a8c9740b8b70b3
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2021-04-23 12:52:48 +0530

    ~sudo~ chmod -x

--- .github/scripts/stan.sh
+++ .github/scripts/stan.sh
@@ -1,4 +1,4 @@
-sudo JULIA_CMDSTAN_HOME="$HOME/cmdstan-2.20.0/"
+JULIA_CMDSTAN_HOME="$HOME/cmdstan-2.20.0/"
 OLDWD=`pwd`
 cd ~
 wget https://github.com/stan-dev/cmdstan/releases/download/v2.20.0/cmdstan-2.20.0.tar.gz

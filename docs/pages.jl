140972b2ed9064161382e53e08aee330597583af
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-06-02 11:21:04 -0400

    Create pages.jl

---
+++ docs/pages.jl
@@ -0,0 +1,5 @@
+pages = [
+        "DiffEqBayes.jl: Bayesian Parameter Estimation for Differential Equations" => "index.md",
+        "Methods" => "methods.md",
+        "Examples" => "examples.md"
+    ]

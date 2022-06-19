f84789696550288b19428da81aea19bb4114e8c9
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2022-06-19 18:51:39 +0530

    Trim supported versions and copy over ci updates from DiffEqBayesStan

--- .github/scripts/stan.sh
+++ .github/scripts/stan.sh
@@ -1,7 +1,13 @@
-JULIA_CMDSTAN_HOME="$HOME/cmdstan-2.26.1/"
+JULIA_CMDSTAN_HOME="/home/runner/cmdstan-2.29.2/"
 OLDWD=`pwd`
 cd ~
-wget https://github.com/stan-dev/cmdstan/releases/download/v2.26.1/cmdstan-2.26.1.tar.gz
-tar -xzpf cmdstan-2.26.1.tar.gz
+pwd
+wget https://github.com/stan-dev/cmdstan/releases/download/v2.29.2/cmdstan-2.29.2.tar.gz
+tar -xzpf cmdstan-2.29.2.tar.gz
+ls -lia .
+ls -lia ./cmdstan-2.29.2
+ls -lia ./cmdstan-2.29.2/make
+touch ./cmdstan-2.29.2/make/local
+echo "STAN_THREADS=true" > ./cmdstan-2.29.2/make/local
 make -C $JULIA_CMDSTAN_HOME build
 cd $OLDWD

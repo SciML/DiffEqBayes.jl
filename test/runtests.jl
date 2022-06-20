4c2329cdaab06408e682927ddcc9eda3c6338031
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-06-20 04:23:59 -0400

    Test ABC

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ const GROUP = get(ENV, "GROUP", "All")
 if GROUP == "All" || GROUP == "Core"
     @time @safetestset "DynamicHMC" begin include("dynamicHMC.jl") end
     @time @safetestset "Turing" begin include("turing.jl") end
-    # @time @safetestset "ABC" begin include("abc.jl") end
+    @time @safetestset "ABC" begin include("abc.jl") end
 end
 
 if GROUP == "Stan" || GROUP == "All"

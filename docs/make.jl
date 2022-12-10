a85f725b8b6c4c112cdba2a33bbec61634ef12f1
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2022-12-10 20:55:38 +0530

    Format

--- docs/make.jl
+++ docs/make.jl
@@ -11,13 +11,13 @@ makedocs(sitename = "DiffEqBayes.jl",
          doctest = false,
          modules = [DiffEqBayes],
          strict = [
-            :doctest,
-            :linkcheck,
-            :parse_error,
-            :example_block,
-            # Other available options are
-            # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
-        ],
+             :doctest,
+             :linkcheck,
+             :parse_error,
+             :example_block,
+             # Other available options are
+             # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+         ],
          format = Documenter.HTML(assets = ["assets/favicon.ico"],
                                   canonical = "https://docs.sciml.ai/DiffEqBayes/stable/"),
          pages = pages)

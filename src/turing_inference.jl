71f1f8ff6652a7f1933a9c2ff4cfb3fde8f2c617
Saumil Shah <shah@evolbio.mpg.de>
2023-06-26 16:04:30 +0200

    modified turing arg structure

--- src/turing_inference.jl
+++ src/turing_inference.jl
@@ -1,21 +1,28 @@
-function turing_inference(prob::DiffEqBase.DEProblem,
-        alg,
-        t,
-        data,
-        priors;
-        likelihood_dist_priors = [InverseGamma(2, 3)],
-        likelihood = (u, p, t, σ) -> MvNormal(u,
-            Diagonal((σ[1])^2 *
-                     ones(length(u)))),
-        num_samples = 1000,
-        sampler = Turing.NUTS(0.65),
-        parallel_type = MCMCSerial(),
-        n_chains = 1,
-        syms = [Turing.@varname(theta[i]) for i in 1:length(priors)],
-        sample_u0 = false,
-        save_idxs = nothing,
-        progress = false,
-        kwargs...)
+using Turing: Tracker
+
+function turing_inference(
+    prob::DiffEqBase.DEProblem,
+    alg,
+    t,
+    data,
+    priors;
+    likelihood_dist_priors = [InverseGamma(2, 3)],
+    likelihood = (u, p, t, σ) -> MvNormal(
+        u, σ[1]^2 * Diagonal(ones(length(u)))
+    ),
+    num_samples = 1000,
+    sampler = Turing.NUTS(0.65),
+    parallel_type = MCMCSerial(),
+    n_chains = 1,
+    syms = [Turing.@varname(theta[i]) for i in 1:length(priors)],
+    sample_u0 = false,
+    progress = false,
+    solve_kwargs = Dict(
+        :save_idxs => nothing,
+    ),
+    sample_kwargs = Dict()
+    )
+
     N = length(priors)
     Turing.@model function infer(x, ::Type{T} = Float64) where {T <: Real}
         theta = Vector{T}(undef, length(priors))
@@ -26,7 +33,7 @@ function turing_inference(prob::DiffEqBase.DEProblem,
         for i in 1:length(likelihood_dist_priors)
             σ[i] ~ likelihood_dist_priors[i]
         end
-        nu = save_idxs === nothing ? length(prob.u0) : length(save_idxs)
+        nu = solve_kwargs[:save_idxs] === nothing ? length(prob.u0) : length(solve_kwargs[:save_idxs])
         u0 = convert.(T, sample_u0 ? theta[1:nu] : prob.u0)
         p = convert.(T, sample_u0 ? theta[(nu + 1):end] : theta)
         if length(u0) < length(prob.u0)
@@ -36,8 +43,7 @@ function turing_inference(prob::DiffEqBase.DEProblem,
             end
         end
         _saveat = t === nothing ? Float64[] : t
-        sol = solve(prob, alg; u0 = u0, p = p, saveat = _saveat, progress = progress,
-            save_idxs = save_idxs, kwargs...)
+        sol = solve(prob, alg; u0 = u0, p = p, saveat = _saveat, progress = progress, solve_kwargs...)
         failure = size(sol, 2) < length(_saveat)
 
         if failure
@@ -62,7 +68,8 @@ function turing_inference(prob::DiffEqBase.DEProblem,
         parallel_type,
         num_samples,
         n_chains;
-        progress = progress
+        progress = progress,
+        sample_kwargs...
     )
     return chn
 end

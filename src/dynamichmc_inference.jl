42c6a18e5b963232f6041b5c4c236f786c83d78c
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-05-31 15:33:22 +0000

    Update dynamichmc_inference.jl

--- src/dynamichmc_inference.jl
+++ src/dynamichmc_inference.jl
@@ -100,22 +100,18 @@ function dynamichmc_inference(problem::DiffEqBase.DEProblem, algorithm, t, data,
         sample_u0 = false, rng = Random.GLOBAL_RNG,
         num_samples = 1000, AD_gradient_kind = Val(:ForwardDiff),
         save_idxs = nothing, solve_kwargs = (),
-
+        mcmc_kwargs = (initialization = (q = zeros(length(parameter_priors) +
+                                           (save_idxs ===
+                                            nothing ?
+                                            length(data[:, 1]) :
+                                            length(save_idxs))),),))
+    
         _p, repack, aliases = if SciMLStructures.isscimlstructure(problem.p)
             canonicalize(SciMLStructures.Tunables(), problem.p)
         else
             problem.p, identity, true
         end
-
-        basevals = zeros(length(parameter_priors)
-        extravals = if save_idxs === nothing
-            length(data[:, 1])
-        else
-            length(save_idxs)
-        end
-        q = basevals + extravals
-        
-        mcmc_kwargs = (; initialization = (; q = q),)
+    
         P = DynamicHMCPosterior(; algorithm = algorithm, problem = problem, t = t, data = data,
         parameter_priors = parameter_priors, σ_priors = σ_priors,
         solve_kwargs = solve_kwargs, sample_u0 = sample_u0,

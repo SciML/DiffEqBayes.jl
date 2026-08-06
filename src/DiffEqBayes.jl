101b5766e73f271c0f75e18edbfbc8fdf1eda113
Arpan Chakraborty <chakrabortyarpan151@gmail.com>
2026-08-07 04:45:09 +0530

    Fix issue #391: explicit imports for DiffEqBayes (#400)
    * Fix issue #391: explicit imports for DiffEqBayes
    
    Changed src/DiffEqBayes.jl to use explicit imports instead of
    implicit ones. Before it was using 'using BigDep' which pulls
    everything in. Now it is 'using BigDep: name1, name2' so only
    the needed names are imported.
    
    Also updated test/qa/qa.jl to remove :no_implicit_imports from
    the broken tests list since the issue is now fixed.
    
    Tested locally:
    - check_no_implicit_imports(DiffEqBayes) passes
    - Full test suite passes (turing 14/14, dynamicHMC 8/10)
    
    * Apply Runic formatting
    
    * Update DiffEqBayes.jl
    
    * Apply Runic formatting
    
    * Remove stale @unpack import
    
    * Add explicit Distributions and StatsAPI imports
    
    * Apply Runic formatting
    
    * Add StatsAPI to dependencies

--- src/DiffEqBayes.jl
+++ src/DiffEqBayes.jl
@@ -3,14 +3,30 @@ $(DocStringExtensions.README)
 """
 module DiffEqBayes
 
-using DiffEqBase, Distributions, Turing, MacroTools
-import SciMLBase
-using RecursiveArrayTools, ModelingToolkit, LinearAlgebra
-using Parameters, Distributions, Optim, Requires
-using Distances, DocStringExtensions, Random, StanSample
-using DynamicHMC, TransformVariables, LogDensityProblemsAD, TransformedLogDensities
-using SciMLStructures
-
+using DiffEqBase: DiffEqBase, EnsembleProblem, SciMLBase, remake
+using Distances: Distances
+using DocStringExtensions: DocStringExtensions, FIELDS, SIGNATURES, TYPEDEF
+using DynamicHMC: DynamicHMC, mcmc_with_warmup
+using LinearAlgebra: LinearAlgebra, Diagonal
+using LogDensityProblemsAD: LogDensityProblemsAD
+using MacroTools: MacroTools
+using ModelingToolkit: ModelingToolkit, parameters, solve
+using Optim: Optim
+using Parameters: Parameters
+using Distributions: Distributions, Bernoulli, Beta, BetaBinomial, Binomial,
+    Categorical, Cauchy, Chisq, Exponential, Frechet, Gamma, GeneralizedPareto,
+    Gumbel, Hypergeometric, Laplace, LogNormal, NegativeBinomial, Pareto,
+    Poisson, Rayleigh, TDist, Truncated, Uniform, VonMises, Weibull
+using StatsAPI: params
+using Random: Random
+using RecursiveArrayTools: RecursiveArrayTools
+using Requires: Requires
+using SciMLStructures: SciMLStructures
+using StanSample: StanSample, SampleModel, read_samples, stan_sample
+using TransformVariables: TransformVariables, as, asℝ₊
+using TransformedLogDensities: TransformedLogDensities, TransformedLogDensity
+using Turing: Turing, InverseGamma, MCMCSerial, MvNormal, NamedDist, Normal,
+    logpdf, sample
 STANDARD_PROB_GENERATOR(prob, p) = remake(prob; u0 = eltype(p).(prob.u0), p = p)
 function STANDARD_PROB_GENERATOR(prob::EnsembleProblem, p)
     return EnsembleProblem(remake(prob.prob; u0 = eltype(p).(prob.prob.u0), p = p))
